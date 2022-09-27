a4b8344db4e6a0b0180cd7a5bc1a6c8331f16955
Nikolay Oreschenko <nickolas1594@mail.ru>
2022-09-27 19:23:10 +0300

    24-todo-app-v1_final

--- src/App.js
+++ src/App.js
@@ -21,16 +21,32 @@ function App() {
 в данной функции мы берем массив деструктуризируем его для
 удобства и доавляем туда наш текст */
 
+  const deleteTodoHandler = (index) => {
+    /* функция которая по двойному клику на задачу
+    удаляет её */
+    setTodos(todos.filter((_, idx) => idx !== index)) 
+    /* первый параметр функции фильтр это 
+    элемент массива вторая индекс 
+    если значение правдиво фильтр все оставляет
+    если нет то то элемент будет удален*/
+
+  }
+
   return (
     <div className="App">
       <h1>Todo App</h1>
       <TodoForm addTodo={addTodoHandler} />
       {/* 1 действие передаем функцию как свойство в файл
       todoform  */}
-      <TodoList todos={todos} />
+      <TodoList todos={todos} deleteTodo={deleteTodoHandler}/>
       {/* 1 действие передаем наш список как переменную в файл TodoList.js */}
+      {/* передаем нашу функцию которая удаляет элемент из массива */}
     </div>
   )
 }
 
 export default App
+/* react-icons ri это раздел на сайте remix icons 
+оттуда мы будем брать наши иконки*/
+
+/* пишем npm install react-icons */
--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,6 +1,21 @@
-function Todo({ todo }) {
+import { RiTodoFill } from 'react-icons/ri'
+/* из внешних пакетов делаем импрот сверху */
+
+import styles from './Todo.module.css'
+
+
+function Todo({ todo, index, deleteTodo }) {
     /* 3 действие принимаем наш список из TodoList и выводим по одной строке */
-  return <h1>{todo}</h1>
+  return (
+    <div className={styles.todo} 
+    onDoubleClick={() => deleteTodo(index)}>
+      {/* по двойному клику вызываем функциб deleteTodo и передаем
+      index из map */}
+      <RiTodoFill className={styles.todoIcon}/>
+      {/* иконка из установленного нпм пакета react icons */}
+      <div className={styles.todoText}>{todo}</div>
+    </div>
+  )
 }
 
 export default Todo
